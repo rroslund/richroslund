f5d6debe7275677ae1f6621977b0f54127b56119
Rich Roslund <rroslund890@gmail.com>
2013-05-09 03:02:53 -0500

    Fixing an error with my last commit. Gotta escape those quotes.

--- src/richroslund/web.clj
+++ src/richroslund/web.clj
@@ -4,7 +4,7 @@
   (:require [compojure.route :as route]))
 
 (defroutes app
-           (GET "/" [] "<html><h1>Hello World</h1><br><a href="http://amaliz.herokuapp.com">Check out Amaliz!</a></html>")
+           (GET "/" [] "<html><head><title>Rich Roslund</title></head><h1>Hello World...and welcome to my site!</h1><p>Please forgive the ghetto feel. This is a side project in Clojure web development. Who knows where it could go! </p><br><a href=\"http://amaliz.herokuapp.com\">Check out Amaliz!</a></html>")
            (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main []
